ce3153ab4d6b8a70dd405dc64734e95d81dfd118
Clay Beal <125436817+clayster4004@users.noreply.github.com>
2023-12-04 00:41:00 -0500

    Update m.py

--- m.py
+++ m.py
@@ -559,7 +559,7 @@ def run(p):
 
 # Real Main
 def main():
-    #print('RAN THIS')
+    print('RAN THIS')
     # Spotify app credentials from your Spotify Developer Dashboard
     SPOTIPY_CLIENT_ID = '2bdfeb8580304b9fb343ff8cc8744e76'
     SPOTIPY_CLIENT_SECRET = '73cbcc49de99490f821c2925c2b41419'
