5bc80ced4970829f560cf6f6eaff1c2440b10a0e
Clay Beal <125436817+clayster4004@users.noreply.github.com>
2023-12-03 23:31:26 -0500

    Update m.py again

--- m.py
+++ m.py
@@ -18,7 +18,7 @@ import os
 # Spotify app credentials from your Spotify Developer Dashboard
 SPOTIPY_CLIENT_ID = '2bdfeb8580304b9fb343ff8cc8744e76'
 SPOTIPY_CLIENT_SECRET = '73cbcc49de99490f821c2925c2b41419'
-SPOTIPY_REDIRECT_URI = 'https://spotifyanalyzertest.streamlit.app'
+SPOTIPY_REDIRECT_URI = 'https://spotifyanalyzertest.streamlit.app/'
 
 # Create a SpotifyOAuth instance
 #sp_oauth = SpotifyOAuth(SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI, scope='user-library-read')
@@ -562,7 +562,7 @@ def main():
     # Spotify app credentials from your Spotify Developer Dashboard
     SPOTIPY_CLIENT_ID = '2bdfeb8580304b9fb343ff8cc8744e76'
     SPOTIPY_CLIENT_SECRET = '73cbcc49de99490f821c2925c2b41419'
-    SPOTIPY_REDIRECT_URI = 'https://spotifyanalyzertest.streamlit.app'
+    SPOTIPY_REDIRECT_URI = 'https://spotifyanalyzertest.streamlit.app/'
     
     st.title("Spotify Playlist Analyzer")
 
@@ -610,7 +610,7 @@ def main():
         sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
             client_id='2bdfeb8580304b9fb343ff8cc8744e76',
             client_secret='73cbcc49de99490f821c2925c2b41419',
-            redirect_uri='https://spotifyanalyzertest.streamlit.app',  # Update the redirect_uri
+            redirect_uri='https://spotifyanalyzertest.streamlit.app/',  # Update the redirect_uri
             scope='playlist-read-private',
             show_dialog=True
         ))
